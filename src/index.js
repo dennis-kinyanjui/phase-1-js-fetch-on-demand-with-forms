fcb7cb2ef83c011ed8cd2c0034fd7c853ccf32d1
Dennis <dennis.kinyanjui@student.moringaschool.com>
2024-04-08 02:29:30 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-const init = () => {
-  
-}
 
-document.addEventListener('DOMContentLoaded', init);
+const init = () => {
+    const inputForm = document.querySelector("form");
+  
+    
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault(); 
+  
+      const input = document.querySelector("#searchByID"); 
+      const movieDetails = document.querySelector("#movieDetails"); 
+      
+      
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          
+          movieDetails.innerHTML = `
+            <h4>${data.title}</h4>
+            <p>${data.summary}</p>
+          `;
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+          movieDetails.innerHTML = "<p>Movie not found!</p>"; 
+        });
+    });
+  };
+  
+  // Event listener for DOM content loaded
+  document.addEventListener("DOMContentLoaded", init);
+  
